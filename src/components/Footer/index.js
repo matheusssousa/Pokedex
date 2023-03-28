c19ed9d3412b1128b471e0c61aefb1bd12e5ddd9
matheusssousa <matheussousab2014@gmail.com>
2023-03-28 18:57:29 -0300

    refactor: navbar and footer styles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,3 @@
-import { GithubLogo, InstagramLogo, LinkedinLogo } from "@phosphor-icons/react";
 import React from "react";
 import "./style.css";
 
@@ -7,17 +6,6 @@ const Footer = () => {
         <div className="footer">
             <div className="info">
                 <p>Feito por Matheus Sousa</p>
-                <nav className="info-icons">
-                    <a href="https://www.instagram.com/theusousab/" target="_blank" rel="noopener noreferrer" className="instagram">
-                        <InstagramLogo size={20} />
-                    </a>
-                    <a href="https://github.com/matheusssousa" target="_blank" rel="noopener noreferrer" className="github">
-                        <GithubLogo size={20} />
-                    </a>
-                    <a href="https://www.linkedin.com/in/matheus-sousa-17b214208/" target="_blank" rel="noopener noreferrer" className="linkedin">
-                        <LinkedinLogo size={20} />
-                    </a>
-                </nav>
             </div>
         </div>
     )
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,25 @@
 import { Moon, Sun } from "@phosphor-icons/react";
 import React from "react";
 import { useTheme } from "../../hooks/useTheme";
+import SocialMedia from "../SocialMedia";
 import "./style.css";
 
 const Navbar = () => {
-    const logoImg = "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png";
+    const logoImg = "https://raw.githubusercontent.com/carlosdancr/pokedex-react/15d991abf17a16d8ad2eb85e48ea4156dc52d75c/src/assets/logo-pokemon.svg";
 
-    const {theme, setTheme} = useTheme();
+    const { theme, setTheme } = useTheme();
 
     return (
         <div className="navbar">
-            <div></div>
             <nav>
-                <img alt="pokeapi-logo" src={logoImg}/>
+                <img alt="pokeapi-logo" src={logoImg} />
             </nav>
-            <div>
-                {theme == "light" ? <Moon size={32} onClick={() => setTheme("dark")} weight="fill" className="buttonmoon"/> : <Sun size={32} onClick={() => setTheme("light")} weight="fill" className="buttonsun"/>}
+            <div className="info-navbar">
+                <div>
+                    {theme == "light" ? <Moon size={25} onClick={() => setTheme("dark")} className="buttonmoon" /> : <Sun size={25} onClick={() => setTheme("light")} className="buttonsun" />}
+                </div>
+                <div className="bar-navbar"/>
+                <SocialMedia />
             </div>
         </div>
     )
