f188142ea10f56e3fa52c4c4c9143421e60d06fa
matheusssousa <matheussousab2014@gmail.com>
2023-03-29 17:58:55 -0300

    scroll pokedex after click pagination

--- src/components/Pokedex/index.js
+++ src/components/Pokedex/index.js
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import Pagination from "../Pagination";
 import Pokemon from "../Pokemon";
 import "./style.css";
 
 const Pokedex = (props) => {
     const { pokemons, loading, page, setPage, totalPages } = props
+    const pokemonListRef = useRef();
 
     const OnPreviousClickHandler = () => {
         if (page > 0) {
             setPage(page - 1)
+            pokemonListRef.current.scrollIntoView({ behavior: "smooth" });
         }
     }
 
     const OnAfterClickHandler = () => {
         if (page + 1 !== totalPages) {
             setPage(page + 1)
+            pokemonListRef.current.scrollIntoView({ behavior: "smooth" });
         }
     }
 
     return (
         <div className="pokedex">
             {loading ? null :
-                (<div className="pokedex-grid">
+                (<div className="pokedex-grid" ref={pokemonListRef}>
                     {pokemons && pokemons.map((pokemon, index) => {
                         return (<Pokemon pokemon={pokemon} key={index} />)
                     })}
